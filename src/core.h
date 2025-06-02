543f321d2ea3d3fa2077597ec1bbf2c5c4160e3d
Kaio Delphino <dash.zet@gmail.com>
2025-06-01 21:57:24 -0300

    feat: define some basic types

---
+++ src/core.h
@@ -0,0 +1,20 @@
+#include <cstdint>
+
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int64_t i64;
+typedef uint64_t u64;
+typedef intptr_t isize;
+typedef uintptr_t usize;
+typedef float f32;
+typedef double f64;
+typedef long double f80;
+
+typedef struct {
+  u32 len;
+  char *head;
+} String;
