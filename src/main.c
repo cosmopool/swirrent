6796bd52eca1a2bc95957e0818ee03050a2d7e4d
Kaio Delphino <dash.zet@gmail.com>
2026-03-26 23:05:27 -0300

    feat: add else clause

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ i32 main(i32 argc, char **argv) {
       continue;
     }
 
-    if (strncmp(argv[i], "--load-response", 15) == 0 || strncmp(argv[i], "-l", 2) == 0) {
+    else if (strncmp(argv[i], "--load-response", 15) == 0 || strncmp(argv[i], "-l", 2) == 0) {
       if (options.raw_request_output_path) {
         printf("you must specify only one of those: '--load-response' or '--dump-response'");
         return 1;
@@ -38,7 +38,7 @@ i32 main(i32 argc, char **argv) {
       continue;
     }
 
-    if (strncmp(argv[i], "--dump-response", 15) == 0 || strncmp(argv[i], "-d", 2) == 0) {
+    else if (strncmp(argv[i], "--dump-response", 15) == 0 || strncmp(argv[i], "-d", 2) == 0) {
       if (options.raw_request_output_path) {
         printf("you must specify only one of those: '--load-response' or '--dump-response'");
         return 1;
@@ -53,12 +53,12 @@ i32 main(i32 argc, char **argv) {
       continue;
     }
 
-    if (strncmp(argv[i], "--decode-only", 13) == 0) {
+    else if (strncmp(argv[i], "--decode-only", 13) == 0) {
       options.decode_only = true;
       continue;
     }
 
-    if (strncmp(argv[i], "-v", 2) == 0 || strncmp(argv[i], "--verbose", 9) == 0) {
+    else if (strncmp(argv[i], "-v", 2) == 0 || strncmp(argv[i], "--verbose", 9) == 0) {
       options.verbose = true;
       continue;
     }
