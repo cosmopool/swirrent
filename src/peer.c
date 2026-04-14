f8e5020ee014c4c4ee0a6e9b435536c1e3174d18
Kaio Delphino <dash.zet@gmail.com>
2026-04-13 21:56:00 -0300

    refactor: move peer logic to separate module

---
+++ src/peer.c
@@ -0,0 +1,180 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <curl/curl.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <unistd.h>
+#ifdef __linux__
+#include <endian.h>
+#else
+#include <sys/endian.h>
+#endif
+
+#include "core.h"
+#include "log.h"
+#include "torrent.h"
+#include "tracker.h"
+
+u32 trackerPeerConnect(i32 fd, struct sockaddr *sock, usize sock_size, char *data, usize data_size) {
+  u32 c = connect(fd, (struct sockaddr *)&sock, sock_size);
+  if (c != 0) {
+    logInfo("connection with peer failed: %s", strerror(errno));
+    return c;
+  }
+
+  if (write(fd, data, data_size) < 0) {
+    logInfo("failed to send data to peer: %s", strerror(errno));
+    return -1;
+  }
+
+  char resp_buff[1024] = {0};
+  if (read(fd, resp_buff, 1023) < 0) {
+    logInfo("failed to read peer response: %s", strerror(errno));
+    return -1;
+  }
+  logInfo("peer response: %s", resp_buff);
+
+  close(fd);
+}
+
+void trackerHandshakeGenerate(u8 *info_hash, u8 *peer_id, char handshake_buff[68]) {
+  assert(info_hash[0] != '\0');
+
+  const char *protocol_str = "BitTorrent protocol";
+  const u32 protocol_len = strlen(protocol_str);
+
+  u32 offset = 0;
+  handshake_buff[offset] = 19;
+  offset++;
+
+  memcpy(handshake_buff + offset, protocol_str, protocol_len);
+  offset += protocol_len;
+  assert(offset == 20);
+
+  memset(handshake_buff + offset, 0, 8);
+  offset += 8;
+  assert(offset == 28);
+
+  memcpy(handshake_buff + offset, info_hash, SHA_DIGEST_LENGTH);
+  offset += SHA_DIGEST_LENGTH;
+  assert(offset == 48);
+
+  memcpy(handshake_buff + offset, peer_id, PEER_ID_LENGTH);
+  offset += PEER_ID_LENGTH;
+  assert(offset == 68);
+  FILE *file = fopen("handshake", "wb");
+  if (file) {
+    // Write some text to the file
+    size_t written = fwrite(handshake_buff, 1, 68, file);
+    if (written < 68) {
+      logInfo("Warning: Only wrote %zu of 68 bytes.", written);
+    }
+  } else {
+    perror("fopen");
+  }
+  // Close the file
+  if (file) fclose(file);
+}
+
+
+u32 trackerPeer4Handshake(TorrentTrackerResponse *resp, u8 *info_hash, u8 *peer_id) {
+  char handshake_buff[68] = {0};
+  trackerHandshakeGenerate(info_hash, peer_id, handshake_buff);
+
+  TorrentPeer peer = torrentPeerGet(resp->peers.data, 0);
+  char ip_str[INET_ADDRSTRLEN] = {0};
+  if (!inet_ntop(AF_INET, peer.ip.data, ip_str, sizeof(ip_str))) {
+    logInfo("  (%d)\t failed to parse ipv: %s", 0, strerror(errno));
+    return -1;
+  }
+
+  u32 result = 0;
+  i32 fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    logInfo("socket error: %s", strerror(errno));
+    return fd;
+  }
+
+  struct sockaddr_in sock = {
+      .sin_port = htons(peer.port),
+      .sin_family = AF_INET,
+  };
+  memcpy(&sock.sin_addr, peer.ip.data, peer.ip.len);
+
+  u32 c = connect(fd, (struct sockaddr *)&sock, sizeof(sock));
+  if (c != 0) {
+    logInfo("connection with peer failed: %s", strerror(errno));
+    result = c;
+    goto cleanup;
+  }
+
+  if (write(fd, handshake_buff, sizeof(handshake_buff)) < 0) {
+    logInfo("failed to send handshake to peer: %s", strerror(errno));
+    result = -1;
+    goto cleanup;
+  }
+
+  char resp_buff[1024] = {0};
+  if (read(fd, resp_buff, sizeof(resp_buff) - 1) < 0) {
+    logInfo("failed to read peer response: %s", strerror(errno));
+    result = -1;
+    goto cleanup;
+  }
+  logInfo("peer response: %s", resp_buff);
+
+cleanup:
+  close(fd);
+  return result;
+}
+
+u32 trackerPeer6Handshake(TorrentTrackerResponse *resp, u8 *info_hash, u8 peer_id[20]) {
+  char handshake_buff[68] = {0};
+  trackerHandshakeGenerate(info_hash, peer_id, handshake_buff);
+
+  TorrentPeer6 peer = torrentPeer6Get(resp->peers6.data, 0);
+  char ip_str[INET6_ADDRSTRLEN] = {0};
+  if (!inet_ntop(AF_INET6, peer.ip.data, ip_str, sizeof(ip_str))) {
+    logInfo("  (%d)\t failed to parse ipv6: %s", 0, strerror(errno));
+    return -1;
+  }
+
+  u32 result = 0;
+  i32 fd = socket(AF_INET6, SOCK_STREAM, 0);
+  if (fd < 0) {
+    logInfo("socket error: %s", strerror(errno));
+    return fd;
+  }
+
+  struct sockaddr_in sock = {
+      .sin_port = htons(peer.port),
+      .sin_family = AF_INET,
+  };
+  memcpy(&sock.sin_addr, peer.ip.data, peer.ip.len);
+
+  u32 c = connect(fd, (struct sockaddr *)&sock, sizeof(sock));
+  if (c != 0) {
+    logInfo("connection with peer failed: %s", strerror(errno));
+    return c;
+  }
+
+  if (send(fd, handshake_buff, sizeof(handshake_buff), 0) < 0) {
+    logInfo("failed to send handshake to peer: %s", strerror(errno));
+    return -1;
+  }
+
+  char resp_buff[1024] = {0};
+  if (recv(fd, resp_buff, sizeof(resp_buff) - 1, 0) < 0) {
+    logInfo("failed to read peer response: %s", strerror(errno));
+    return -1;
+  }
+  logInfo("peer response: %s", resp_buff);
+
+  close(fd);
+  return result;
+}
--- src/tracker.c
+++ src/tracker.c
@@ -136,7 +136,7 @@ void parse_tracker_url(String url, char *host, size_t host_len, char *port, size
     strncpy(port, port_start, port_size);
     port[port_size] = '\0';
   } else {
-    strcpy(port, "80"); // default
+    strcpy(port, "80");
   }
 }
 
@@ -177,9 +177,6 @@ i32 trackerAnnounceStart(u8 info_hash[20], u32 fd, u8 peer_id[20]) {
 i32 trackerAnnounceFinish(u32 fd) {
   u8 buff[2048] = {0};
   TrackerAnnounceResponse *response = (TrackerAnnounceResponse *)buff;
-  // TrackerAnnounceResponse *response = malloc(2048);
-  // bzero(response, 2048);
-
   struct sockaddr from;
   socklen_t from_len;
   isize bytes_read = recvfrom(fd, response, sizeof(buff), MSG_WAITALL, &from, &from_len);
@@ -225,9 +222,6 @@ i32 trackerAnnounceFinish(u32 fd) {
   logInfo("");
 
   return 0;
-  // cleanup:
-  //   if (response != NULL) free(response);
-  //   return -1;
 }
 
 i32 trackerResolveAddress(void *url, void **out) {
@@ -341,163 +335,6 @@ i32 trackerConnectionFinish(i32 fd) {
   return 0;
 }
 
-void trackerHandshakeGenerate(u8 *info_hash, u8 *peer_id, char handshake_buff[68]) {
-  assert(info_hash[0] != '\0');
-
-  const char *protocol_str = "BitTorrent protocol";
-  const u32 protocol_len = strlen(protocol_str);
-
-  u32 offset = 0;
-  handshake_buff[offset] = 19;
-  offset++;
-
-  memcpy(handshake_buff + offset, protocol_str, protocol_len);
-  offset += protocol_len;
-  assert(offset == 20);
-
-  memset(handshake_buff + offset, 0, 8);
-  offset += 8;
-  assert(offset == 28);
-
-  memcpy(handshake_buff + offset, info_hash, SHA_DIGEST_LENGTH);
-  offset += SHA_DIGEST_LENGTH;
-  assert(offset == 48);
-
-  memcpy(handshake_buff + offset, peer_id, PEER_ID_LENGTH);
-  offset += PEER_ID_LENGTH;
-  assert(offset == 68);
-  FILE *file = fopen("handshake", "wb");
-  if (file) {
-    // Write some text to the file
-    size_t written = fwrite(handshake_buff, 1, 68, file);
-    if (written < 68) {
-      logInfo("Warning: Only wrote %zu of 68 bytes.", written);
-    }
-  } else {
-    perror("fopen");
-  }
-  // Close the file
-  if (file) fclose(file);
-}
-
-// u32 trackerPeerConnect(i32 fd, struct sockaddr *sock, usize sock_size, char *data, usize data_size) {
-//   u32 c = connect(fd, (struct sockaddr *)&sock, sock_size);
-//   if (c != 0) {
-//     logInfo("connection with peer failed: %s", strerror(errno));
-//     return c;
-//   }
-//
-//   if (write(fd, data, data_size) < 0) {
-//     logInfo("failed to send data to peer: %s", strerror(errno));
-//     return -1;
-//   }
-//
-//   char resp_buff[1024] = {0};
-//   if (read(fd, resp_buff, 1023) < 0) {
-//     logInfo("failed to read peer response: %s", strerror(errno));
-//     return -1;
-//   }
-//   logInfo("peer response: %s", resp_buff);
-//
-//   close(fd);
-// }
-
-u32 trackerPeer4Handshake(TorrentTrackerResponse *resp, u8 *info_hash, u8 *peer_id) {
-  char handshake_buff[68] = {0};
-  trackerHandshakeGenerate(info_hash, peer_id, handshake_buff);
-
-  TorrentPeer peer = torrentPeerGet(resp->peers.data, 0);
-  char ip_str[INET_ADDRSTRLEN] = {0};
-  if (!inet_ntop(AF_INET, peer.ip.data, ip_str, sizeof(ip_str))) {
-    logInfo("  (%d)\t failed to parse ipv: %s", 0, strerror(errno));
-    return -1;
-  }
-
-  u32 result = 0;
-  i32 fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0) {
-    logInfo("socket error: %s", strerror(errno));
-    return fd;
-  }
-
-  struct sockaddr_in sock = {
-      .sin_port = htons(peer.port),
-      .sin_family = AF_INET,
-  };
-  memcpy(&sock.sin_addr, peer.ip.data, peer.ip.len);
-
-  u32 c = connect(fd, (struct sockaddr *)&sock, sizeof(sock));
-  if (c != 0) {
-    logInfo("connection with peer failed: %s", strerror(errno));
-    result = c;
-    goto cleanup;
-  }
-
-  if (write(fd, handshake_buff, sizeof(handshake_buff)) < 0) {
-    logInfo("failed to send handshake to peer: %s", strerror(errno));
-    result = -1;
-    goto cleanup;
-  }
-
-  char resp_buff[1024] = {0};
-  if (read(fd, resp_buff, sizeof(resp_buff) - 1) < 0) {
-    logInfo("failed to read peer response: %s", strerror(errno));
-    result = -1;
-    goto cleanup;
-  }
-  logInfo("peer response: %s", resp_buff);
-
-cleanup:
-  close(fd);
-  return result;
-}
-
-u32 trackerPeer6Handshake(TorrentTrackerResponse *resp, u8 *info_hash, u8 peer_id[20]) {
-  char handshake_buff[68] = {0};
-  trackerHandshakeGenerate(info_hash, peer_id, handshake_buff);
-
-  TorrentPeer6 peer = torrentPeer6Get(resp->peers6.data, 0);
-  char ip_str[INET6_ADDRSTRLEN] = {0};
-  if (!inet_ntop(AF_INET6, peer.ip.data, ip_str, sizeof(ip_str))) {
-    logInfo("  (%d)\t failed to parse ipv6: %s", 0, strerror(errno));
-    return -1;
-  }
-
-  u32 result = 0;
-  i32 fd = socket(AF_INET6, SOCK_STREAM, 0);
-  if (fd < 0) {
-    logInfo("socket error: %s", strerror(errno));
-    return fd;
-  }
-
-  struct sockaddr_in sock = {
-      .sin_port = htons(peer.port),
-      .sin_family = AF_INET,
-  };
-  memcpy(&sock.sin_addr, peer.ip.data, peer.ip.len);
-
-  u32 c = connect(fd, (struct sockaddr *)&sock, sizeof(sock));
-  if (c != 0) {
-    logInfo("connection with peer failed: %s", strerror(errno));
-    return c;
-  }
-
-  if (send(fd, handshake_buff, sizeof(handshake_buff), 0) < 0) {
-    logInfo("failed to send handshake to peer: %s", strerror(errno));
-    return -1;
-  }
-
-  char resp_buff[1024] = {0};
-  if (recv(fd, resp_buff, sizeof(resp_buff) - 1, 0) < 0) {
-    logInfo("failed to read peer response: %s", strerror(errno));
-    return -1;
-  }
-  logInfo("peer response: %s", resp_buff);
-
-  close(fd);
-  return result;
-}
-
 TorrentTrackerResponse trackerHttpFetch(CURL *curl, String tracker_url, TorrentMetainfo *metainfo, u8 *peer_id) {
   assert(tracker_url.data[0] == 'h');
   assert(tracker_url.data[1] == 't');
@@ -568,7 +405,6 @@ void freeTrackerState(TrackerState *t) {
 
 void trackerStateResolver(i32 fd, void *m, u8 peer_id[20]) {
   TorrentMetainfo *metainfo = m;
-
   i32 tracker_idx = fd_to_tracker[fd];
   logInfo("===| tracker (%d)", trackers[tracker_idx].id);
   switch (trackers[tracker_idx].action) {
@@ -651,7 +487,6 @@ u32 trackerPeerListFetch(TorrentMetainfo *metainfo, TorrentTrackerResponse *out,
     logInfo("\n===| tracker (%d) url: %.*s", j, (u32)url.len, url.data);
     bool is_udp = url.data[0] == 'u' && url.data[1] == 'd' && url.data[2] == 'p';
     if (!is_udp) continue;
-
     remaining++;
     ThreadJob job = {.tracker_id = j, .args = metainfo->trackers_url + j, .callback = trackerResolveAddress};
     threadsJobEnqueue(job);
@@ -671,7 +506,6 @@ u32 trackerPeerListFetch(TorrentMetainfo *metainfo, TorrentTrackerResponse *out,
       ASSERT(remaining == 0, "all trackers should have being processed by now");
       break;
     }
-
     remaining--;
     if (job.error != 0) {
       logInfo("not able to resolve tracker (%d) address. failed with error: %d", job.tracker_id, job.error);
