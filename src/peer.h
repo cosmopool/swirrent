768eea67e26accdd72d54c4515a2ef1a0304959c
Kaio Delphino <dash.zet@gmail.com>
2026-04-17 12:24:19 -0300

    feat: implement PeerStatus

--- src/peer.h
+++ src/peer.h
@@ -1,12 +1,26 @@
+#include <stdbool.h>
+#include <sys/socket.h>
 #ifdef __linux__
 #include <endian.h>
-#include <socket.h>
 #else
-#include <sys/socket.h>
+#include <sys/endian.h>
 #endif
 
 #include "core.h"
 #include "torrent.h"
+
+typedef enum {
+  CHOKED,
+  UNCHOKED,
+  INTERESTED,
+  NOT_INTERESTED,
+} PEER_STATUS;
+
+typedef struct {
+  PEER_STATUS their_status;
+  PEER_STATUS our_status;
+  u32 bitfield;
+} PeerStatus;
 
 void peerHandshakeGenerate(u8 *info_hash, u8 *peer_id, char handshake_buff[68]);
 u32 peerConnect(i32 fd, struct sockaddr *sock, usize sock_size, char *data, usize data_size);
