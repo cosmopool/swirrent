983bed2e279824af9055389408d3553a10870aa3
Kaio Delphino <dash.zet@gmail.com>
2026-04-17 20:33:25 -0300

    feat: enable treeshaking for unix builds

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CFLAGS="-g -O0 -Wall -Werror -Wcast-align -Wunreachable-code"
+CFLAGS="-g -O0 -Wall -Werror -Wcast-align -Wunreachable-code -flto"
 INCLUDES="-lcurl"
 SOURCES="src/unix/* src/swirrent.c src/bencode.c src/torrent.c src/tracker.c src/asio.c src/log.c src/peer.c"
 echo "clang -o decoder $CFLAGS $INCLUDES $SOURCES"
