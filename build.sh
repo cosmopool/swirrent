3bd7a842b234b98ba2b2ecf0817c4b97a2623889
Kaio Delphino <dash.zet@gmail.com>
2026-04-23 09:52:35 -0300

    fix: won't build on linux

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CFLAGS="-g -O0 -Wall -Werror -Wcast-align -Wunreachable-code -flto"
+CFLAGS="-g -O0 -Wall -Werror -Wcast-align -Wunreachable-code"
 INCLUDES="-lcurl"
 SOURCES="src/unix/* src/swirrent.c src/bencode.c src/torrent.c src/tracker.c src/asio.c src/log.c src/peer.c"
 echo "clang -o swirrent $CFLAGS $INCLUDES $SOURCES"
